cccf34f257ebb65bc9b771447ee7d85f01d02ac1
vvoot93 <137525312+vvoot93@users.noreply.github.com>
2024-08-29 17:04:19 +0530

    new corrections are added

--- signaling-server.js
+++ signaling-server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
@@ -26,6 +30,7 @@ io.on('connection', (socket) => {
     });
 });
 
+// Start the server on port 3000
 server.listen(3000, () => {
     console.log('Signaling server is running on port 3000');
 });
